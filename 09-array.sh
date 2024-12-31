0deb8dcd61430f068f9d5a38896366097d0871c1
Naishikacharan <venkata5797@gmail.com>
2024-12-31 15:44:31 +0530

    sum of 2 numbers

--- 09-array.sh
+++ 09-array.sh
@@ -1,8 +1,10 @@
-#!bin/bash/
+#!/bin/bash
 
-MOVIES=("PUSHPA" "RRR" "DEVARA" "KUSHI")
-echo"First movie: ${MOVIES[0]}"
-echo"Second movie: ${MOVIES[1]}"
-echo"Third movie: ${MOVIES[2]}"
-echo "Fourth movie: ${MOVIES[3]}"
-echo "All Movies are:${MOVIES[@]}"
+MOVIES=("pushpa" "rrr" "devara")
+# index starts from 0, size is 3
+
+echo "First movie: ${MOVIES[0]}"
+echo "Second movie: ${MOVIES[1]}"
+echo "Third movie: ${MOVIES[2]}"
+
+echo "All movies are: ${MOVIES[@]}"
