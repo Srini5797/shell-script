8d1b3a8dc95c11f5005a86c6e8fb485611a06261
Naishikacharan <venkata5797@gmail.com>
2025-01-07 17:12:13 +0530

    crontab

--- 19-diskusage.sh
+++ 19-diskusage.sh
@@ -5,7 +5,7 @@ DISK_THRESHHOLD=5
 
 while read -r line
 do 
-   USAGE=$(echo $line |awk -F " " '{print$6F}' cut -d "%" -f1)
+   USAGE=$(echo $line | awk -F " " '{print$6F}' | cut -d "%" -f1)
    PARTITION=$(echo $line | awk -F " " '{print $NF}')
    echo "Partition: $PARTITION , Usage: $USAGE"
 
